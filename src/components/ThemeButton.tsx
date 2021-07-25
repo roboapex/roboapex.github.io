98a233041847a4f087a94b87ac92b0a18fd59601
Ryan Theodore The <ryan.the.2006@gmail.com>
2021-07-25 17:47:51 +0800

    Fix broken links baseurl

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { Link } from "@docusaurus/router";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
-export default function ThemeButton({path, children}: {path: string, children: string}) {
+export default function ThemeButton({
+  path,
+  children,
+}: {
+  path: string;
+  children: string;
+}) {
   return (
-    <Link className="button button--secondary button--lg" to={path} style={{ backgroundColor: "#fff" }}>
+    <Link
+      className="button button--secondary button--lg"
+      to={useBaseUrl(path)}
+      style={{ backgroundColor: "#fff" }}
+    >
       {children}
     </Link>
   );
