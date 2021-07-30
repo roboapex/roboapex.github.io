f00aefd8009631d033ecec73b6d7d276a041c0ef
Ryan Theodore The <ryan.the.2006@gmail.com>
2021-07-30 14:31:39 +0800

    Add achievements page

---
+++ src/pages/achievements/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import WorkInProgress from "../../components/workInProgress";
+import Layout from "@theme/Layout";
+
+export default function AchievementsPage() {
+  return (
+    <Layout title="Achievements">
+      <WorkInProgress />
+    </Layout>
+  );
+}
