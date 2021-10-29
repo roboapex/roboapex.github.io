7c8647de1fd6ca5f91fae8ab26a6f67a363ce409
Ryan Theodore The <ryan.the.2006@gmail.com>
2021-10-29 22:48:19 +0800

    Update achievements

---
+++ src/types/ac.ts
@@ -0,0 +1,427 @@
+import {Achievements} from "./achievements"
+
+const x: Achievements[] = [
+  {
+    year: 2021,
+    competitions: [
+      {
+        code: "rcsg",
+        desc: "",
+        awards: [
+          {
+            title: "2nd Place",
+            category: "Technical Challenge and Merit, Presentation, RoboCupJunior Rescue Line (U19)",
+            team: "HexaMatrix",
+            project: "",
+            recipients: [
+              "Koh Yi Lei, Elliot",
+              "Ryan Theodore The",
+              "Koh Jie Ming, Xavier",
+              "Javier Lim"
+            ],
+            media: [
+              {
+                caption: "Rick Astley",
+                url: "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024"
+              }
+            ]
+          },
+          {
+            title: "3rd Place",
+            category: "RCAP CoSpace Autonomous Driving (U19)",
+            team: "SSTS",
+            project: "",
+            recipients: ["Aathithya Jegatheesan"]
+          },
+          {
+            title: "Distinction",
+            category: "Presentation, RoboCupJunior Rescue Line (U19)",
+            team: "iso ohms",
+            project: "",
+            recipients: [
+              "Shanjiith Pranov Gayathri Vijayaraghavan",
+              "Wong Le Yang",
+              "Lee Wei Hao Andy",
+              "Ang Kai Jie, Jovan"
+            ]
+          },
+          {
+            title: "Distinction",
+            category: "Presentation, RoboCupJunior Rescue Line (U19)",
+            team: "I(am)soedgy",
+            project: "",
+            recipients: [
+              "Lim Meng Shin",
+              "Seng Kay Kang, Jerick",
+              "Brayden Ziyang Saga"
+            ]
+          },
+          {
+            title: "Merit",
+            category: "Presentation, RoboCupJunior Rescue Line (U19)",
+            team: "Polyneurons",
+            project: "",
+            recipients: [
+              "Bu Ping Jin",
+              "Kunwar Khushi Karansingh",
+              "Ysabelle Wong Sze Han",
+              "Phang Qian Ci"
+            ]
+          }
+        ],
+        media: []
+      },
+      {
+        code: "fll",
+        desc: "The theme of FLL is Replay and the main idea behind it is healthy living, where competitors have to participate in a robot mission and create an innovation product to help people be more active while having fun",
+        awards: [
+          {
+            title: "3rd Place",
+            category: "Champions Award (Secondary)",
+            team: "Polygamers",
+            project: "",
+            recipients: [
+              "Vijaya Bhargavi",
+              "Soin Wei Dong",
+              "Ang Shaal, Josh"
+            ]
+          },
+          {
+            title: "4th Place",
+            category: "Robot Performance Award (Secondary)",
+            team: "Polyclone",
+            project: "",
+            recipients: [
+              "Irman Zafyree Bin Irmanizam",
+              "Chin Jian Wen",
+              "Favian Loh Dai Yong",
+              "Leow Kar Lok"
+            ]
+          }
+        ],
+        media: []
+      }
+    ]
+  },
+  {
+    year: 2020,
+    competitions: []
+  },
+  {
+    year: 2019,
+    competitions: []
+  },
+  {
+    year: 2018,
+    competitions: []
+  },
+  {
+    year: 2017,
+    competitions: []
+  },
+  {
+    year: 2016,
+    competitions: []
+  },
+  {
+    year: 2015,
+    competitions: [
+      {
+        code: "bullseye",
+        desc: "",
+        awards: [
+          {
+            title: "Silver Award",
+            category: "Category B",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Best Decoration",
+            category: "Category B",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          }
+        ]
+      },
+      {
+        code: "nrc",
+        desc: "(Known as the National Junior Robotics Competition during time of competition)",
+        awards: [
+          {
+            title: "1st Place",
+            category: "Best Presentation Award",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "3rd Place",
+            category: "Best Learning Journey award",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "1st Place",
+            category: "Most Bizarre Robot Design Award",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          }
+        ]
+      },
+      {
+        code: "vexsg",
+        desc: "",
+        awards: [
+          {
+            title: "1st Place",
+            category: "Overall School Ranking",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "1st Place",
+            category: "Match Alliance",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "2nd Place",
+            category: "Match Alliance",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "3rd Place",
+            category: "Match Alliance",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "3rd Place",
+            category: "Robot Skills",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Design Award",
+            category: "",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+        ]
+      },
+      {
+        code: "apyrc",
+        desc: "",
+        awards: [
+          {
+            title: "Champion",
+            category: "Overall",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Champion",
+            category: "Secondary School",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Champion",
+            category: "Creative Challenge",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Champion",
+            category: "Flying Fox Challenge",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "2nd Runner Up",
+            category: "Flying Fox Challenge",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Champion",
+            category: "Mission Programming Challenge",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "1st Runner Up",
+            category: "Mission Programming Challenge",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "2nd Runner Up",
+            category: "Mission Programming Challenge",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "2nd Runner Up",
+            category: "Sumo Challenge",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+        ]
+      },
+      {
+        code: "vexworld",
+        desc: "",
+        awards: [
+          {
+            title: "2nd Place",
+            category: "Division Ranking",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Semi-finalists",
+            category: "Division Ranking",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Sportsmanship Award",
+            category: "",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Service Award",
+            category: "",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "Judges Award",
+            category: "",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+        ]
+      },
+      {
+        code: "fll",
+        desc: "",
+        awards: [
+          {
+            title: "1st Place",
+            category: "Presentation (Project Awards)",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          },
+          {
+            title: "1st Place",
+            category: "Innovative Solution (Project Awards)",
+            team: "",
+            project: "",
+            recipients: []
+          },
+          {
+            title: "2nd Runner-Up",
+            category: "Research Design (Project Awards)",
+            team: "",
+            project: "",
+            recipients: []
+          }
+        ],
+        media: []
+      }
+    ]
+  },
+  {
+    year: 2014,
+    competitions: []
+  },
+  {
+    year: 2013,
+    competitions: []
+  },
+  {
+    year: 2012,
+    competitions: []
+  },
+  {
+    year: 2011,
+    competitions: []
+  },
+  {
+    year: 2010,
+    competitions: [
+      {
+        code: "nrc",
+        desc: "(Known as the National Junior Robotics Competition during time of competition)",
+        awards: [
+          {
+            title: "2nd place, Best Research Award",
+            category: "",
+            team: "",
+            project: "",
+            recipients: [],
+            media: []
+          }
+        ]
+      }
+    ]
+  },
+]
+
+export default x
--- src/types/competitions.ts
+++ src/types/competitions.ts
@@ -41,8 +41,35 @@ export const CompetitionCodes: { [k: CompetitionCode]: Competition } = {
     name: "Asia Pacific Youth Robotics Competition",
     website: "https://apyrc.com/",
     categories: {
-      sumo: "",
-      mission: ""
+      
+    }
+  },
+  vexsg: {
+    name: "Singapore VEX Robotics Competition",
+    website: "",
+    categories: {
+      
+    }
+  },
+  vexap: {
+    name: "Asia-Pacific VEX Robotics Championship",
+    website: "",
+    categories: {
+      
+    }
+  },
+  vexworld: {
+    name: "VEX Robotics World Championship",
+    website: "",
+    categories: {
+      
+    }
+  },
+  bullseye: {
+    name: "Bull’s Eye Competition",
+    website: "https://victoria.moe.edu.sg/bulls-eye-competition/",
+    categories: {
+      
     }
   }
 };
