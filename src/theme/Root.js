4bcbccd8cb9e0935270ed3e3145930e0d51d793a
Ryan Theodore The <ryan.the.2006@gmail.com>
2021-12-22 17:53:36 +0800

    Update meta keywords

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -6,6 +6,24 @@ export default function Root({ children }) {
   return (
     <>
       <Head>
+        <meta property="og:title" content="Robotics @APEX" />
+        <meta
+          property="og:description"
+          content="Robotics @APEX website."
+        />
+        <meta property="og:image" content="./img/og:image.jpg" />
+        <meta property="og:url" content="https://roboapex.github.io" />
+
+        <meta name="version" content="v1.0" />
+        <meta
+          name="description"
+          content="Robotics @APEX website."
+        />
+        <meta name="subject" content="Robotics @APEX" />
+        <meta
+          name="keywords"
+          content="SST, robotics, SST Robotics, CCA, coding, programming, robots"
+        />
         <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
       </Head>
       <SWRConfig value={{
