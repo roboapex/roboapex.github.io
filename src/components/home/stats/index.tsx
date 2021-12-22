35ca426fff8a43e7847392c07e32964d1e079d3b
Ryan Theodore The <ryan.the.2006@gmail.com>
2021-12-22 19:36:32 +0800

    Update home page stats

--- src/components/home/stats/index.tsx
+++ src/components/home/stats/index.tsx
@@ -1,16 +1,29 @@
 import ThemeButton from "../../theme/button";
 import React from "react";
 import style from "./style.module.scss";
+import clsx from "clsx";
 
 export default function HomeStats() {
   return (
     <section className={style.main}>
-      <div className={style.circle}>
-        <h1 className={style.num}>&gt;100</h1>
-        <h2>awards</h2>
+      <div className={style.circles}>
+      <div className={style.yellow}>
+          <h1>&gt;200</h1>
+          <h2>projects</h2>
+        </div>
+        <div className={style.red}>
+          <h1>&gt;100</h1>
+          <h2>awards</h2>
+        </div>
+        <div className={style.blue}>
+          <h2>over</h2>
+          <h1>&gt;10</h1>
+          <h2>competitions</h2>
+        </div>
       </div>
-      <ThemeButton path='/achievements' color="secondary">Learn More</ThemeButton>
-
+      <ThemeButton path="/achievements" color="secondary">
+        Learn More
+      </ThemeButton>
     </section>
   );
 }
