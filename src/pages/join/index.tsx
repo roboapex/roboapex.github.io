bbf0e3ce2bc294983babb987ea8f2ad5d282c81c
Ryan The <>
2021-12-30 15:20:28 +0800

    Remove /join notice

--- src/pages/join/index.tsx
+++ src/pages/join/index.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import ThemeLayout from "../../components/theme/layout";
-import Notice from "../../components/notice";
 
 // TEMP: url for applications
-export const joinURL = "https://docs.google.com/forms/d/e/1FAIpQLScpremOj-MTsoWd-zYJEvQ08l4NjS8mC_Cyps7TESTU313aAQ/viewform?usp=sf_link"
+export const joinURL =
+  "https://docs.google.com/forms/d/e/1FAIpQLScpremOj-MTsoWd-zYJEvQ08l4NjS8mC_Cyps7TESTU313aAQ/viewform?usp=sf_link";
 
 export default function JoinPage() {
   return (
-    <ThemeLayout title="Join">
-      <meta http-equiv="refresh" content={`0; URL=${joinURL}`} />
-      <Notice>If you are not redirected immediately, click here: <a href={joinURL}>{joinURL}</a></Notice>
-      {/* <Notice>Applications will open soon! Save this URL and check back soon!</Notice> */}
-    </ThemeLayout>
+    <meta http-equiv="refresh" content={`0; URL=${joinURL}`} />
   );
 }
+
+
+//<Redirect to={joinURL}></Redirect>
+//<ThemeLayout title="Join">
+//<div>
+//{(() => {
+//  window.location.href = joinURL;
+//  return null;
+//})()}
+//</div>
+//
+//<Notice>If you are not redirected immediately, click here: <a href={joinURL}>{joinURL}</a></Notice>
+//{/* <Notice>Applications will open soon! Save this URL and check back soon!</Notice> */}
+//</ThemeLayout>
