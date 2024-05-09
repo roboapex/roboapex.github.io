8c89584c32f7ad7d8ee5eb8412ff2a8737a9a8ab
Yau Shing <75722100+yaushing@users.noreply.github.com>
2024-05-09 14:55:55 +0800

    test

---
+++ src/pages/licensing/index.tsx
@@ -0,0 +1,390 @@
+import React from "react";
+
+import style from "./style.module.scss";
+
+export default function HomePage() {
+
+  return (
+    <ThemeLayout title="Home" noPadding>
+      <h1>The Robotics@Apex License</h1>
+      <pre>
+      ROBOTICS@APEX LICENSE
+                       Version 1, 9 May 2024
+
+ Copyright (C) 2024 Robotics@Apex, [roboapex.github.io](roboapex.github.io)
+
+Only staff, students and alumni of the School of Science and Technology, 
+Singapore are permitted to copy and distribute verbatim copies of this 
+license document, but changing it is strictly not allowed.
+
+                            Description:
+
+  The Robotics@Apex License is a free, 
+copyleft license for software and other kinds of works.
+
+  Robotics@Apex has numerous publically available 
+passion projects and codes for local and global robotics competitions such as the 
+[First Lego League](https://www.firstlegoleague.org), and the [World Robot Olympiad](https://wro-association.org). As such,
+While many free open source licenses are designed to guarantee the freedom of a program,
+the Robotics@Apex License is intended to protect the works of members of Robotics@Apex,
+and ensure the rights of the developers.
+
+  When we speak of free software, we are referring to freedom, not
+price.  The Robotics@Apex License is designed to make sure that only the
+developer(s) have the freedom to distribute copies of free software 
+(provided they do not charge for it), that they receive source code or can 
+get it if they want it, that they can change the software or use pieces of it in new
+free programs, and that they know they can do these things.
+
+  To protect your rights, we need to prevent others from denying you
+these rights or asking you to surrender the rights.  Therefore, you have
+certain responsibilities if you distribute copies of the software, or if
+you modify it: responsibilities to respect the freedom of others.
+
+  For example, if you distribute copies of such a program, whether
+gratis or for a fee, you must pass on to the recipients the same
+freedoms that you received.  You must make sure that they, too, receive
+or can get the source code.  And you must show them these terms so they
+know their rights.
+
+  Developers that use the GNU GPL protect your rights with two steps:
+(1) assert copyright on the software, and (2) offer you this License
+giving you legal permission to copy, distribute and/or modify it.
+
+  For the developers' and authors' protection, the GPL clearly explains
+that there is no warranty for this free software.  For both users' and
+authors' sake, the GPL requires that modified versions be marked as
+changed, so that their problems will not be attributed erroneously to
+authors of previous versions.
+
+  Some devices are designed to deny users access to install or run
+modified versions of the software inside them, although the manufacturer
+can do so.  This is fundamentally incompatible with the aim of
+protecting users' freedom to change the software.  The systematic
+pattern of such abuse occurs in the area of products for individuals to
+use, which is precisely where it is most unacceptable.  Therefore, we
+have designed this version of the GPL to prohibit the practice for those
+products.  If such problems arise substantially in other domains, we
+stand ready to extend this provision to those domains in future versions
+of the GPL, as needed to protect the freedom of users.
+
+  Finally, every program is threatened constantly by software patents.
+States should not allow patents to restrict development and use of
+software on general-purpose computers, but in those that do, we wish to
+avoid the special danger that patents applied to a free program could
+make it effectively proprietary.  To prevent this, the GPL assures that
+patents cannot be used to render the program non-free.
+
+  The precise terms and conditions for copying, distribution and
+modification follow.
+
+                       TERMS AND CONDITIONS
+
+1. Definitions.
+
+  "This License" refers to version 1 of the Robotics@Apex License.
+
+  "Copyright" also means copyright-like laws that apply to other kinds of
+works, such as semiconductor masks.
+
+  "The Program" refers to any copyrightable work licensed under this
+License.  Each licensee is addressed as "you".  "Licensees" and
+"recipients" may be individuals or organizations.
+
+  To "modify" a work means to copy from or adapt all or part of the work
+in a fashion requiring copyright permission, other than the making of an
+exact copy.  The resulting work is called a "modified version" of the
+earlier work or a work "based on" the earlier work.
+
+  A "covered work" means either the unmodified Program or a work based
+on the Program.
+
+  To "propagate" a work means to do anything with it that, without
+permission, would make you directly or secondarily liable for
+infringement under applicable copyright law, except executing it on a
+computer or modifying a private copy.  Propagation includes copying,
+distribution (with or without modification), making available to the
+public, and in some countries other activities as well.
+
+  To "convey" a work means any kind of propagation that enables other
+parties to make or receive copies.  Mere interaction with a user through
+a computer network, with no transfer of a copy, is not conveying.
+
+1. Source Code
+  The "source code" for a work means the preferred form of the work for 
+making modifications to it. For a software program, this includes the 
+complete set of its source code modules, compiler input files, scripts 
+used to control compilation and installation of the executable program, and 
+interface definitions used by scripts. Source code need not include compilation 
+or assembly instructions or machine-executable binary files, provided that those 
+are directly usable in the final edited form.
+
+2. Basic Permissions
+
+  All rights granted under this License are granted for the term of
+copyright on the Program, provided the stated conditions are met. This
+License explicitly affirms your unlimited permission to run the unmodified
+Program, provided attribution is given.
+
+  You may make, run and propagate covered works that you do not
+convey, without conditions so long as your license otherwise remains
+in force.  You may convey covered works to others for the sole purpose
+of having them make modifications exclusively for you, provided that 
+there exists documented signed permission for the modifications of the software.  
+Those thus making or running the covered works for you must do so exclusively on 
+your behalf, under your direction and control, on terms that prohibit them from 
+making any copies of your copyrighted material outside their relationship with you.
+
+  Conveying under any other circumstances is permitted solely under
+the conditions stated below.  Sublicensing is not allowed; section 10
+makes it unnecessary.
+
+3. Conveying Verbatim Copies
+
+  You may convey verbatim copies of the Program's source code, in any medium,
+provided that you conspicuously and appropriately publish on each copy an appropriate 
+copyright notice; keep intact all notices stating that this License and any
+non-permissive terms added in accord with section 7 apply to the code;
+keep intact all notices of the absence of any warranty; and give all
+recipients a copy of this License along with the Program.
+
+  You may not charge a price for any copy that you convey.
+
+
+
+4. Conveying Modified Source Versions.
+
+  You may convey a work based on the Program, or the modifications to
+produce it from the Program, in the form of source code under the
+terms of section 3, provided that you also meet all of these conditions:
+
+    a) The work must carry prominent notices stating that you modified
+    it, and giving a relevant date.
+
+    b) The work must carry prominent notices stating that it is
+    released under this License and any conditions added under section
+    7.  This requirement modifies the requirement in section 4 to
+    "keep intact all notices".
+
+    c) You must license the entire work, as a whole, under this
+    License to anyone who comes into possession of a copy.  This
+    License will therefore apply, along with any applicable section 7
+    additional terms, to the whole of the work, and all its parts,
+    regardless of how they are packaged.  This License gives no
+    permission to license the work in any other way, but it does not
+    invalidate such permission if you have separately received it.
+
+    d) If the work has interactive user interfaces, each must display
+    Appropriate Legal Notices; however, if the Program has interactive
+    interfaces that do not display Appropriate Legal Notices, your
+    work need not make them do so.
+
+  A compilation of a covered work with other separate and independent
+works, which are not by their nature extensions of the covered work,
+and which are not combined with it such as to form a larger program,
+in or on a volume of a storage or distribution medium, is called an
+"aggregate" if the compilation and its resulting copyright are not
+used to limit the access or legal rights of the compilation's users
+beyond what the individual works permit.  Inclusion of a covered work
+in an aggregate does not cause this License to apply to the other
+parts of the aggregate.
+
+5. Additional Terms
+
+"Additional permissions" are terms that supplement the terms of this
+License by making exceptions from one or more of its conditions.
+Additional permissions that are applicable to the entire Program shall
+be treated as though they were included in this License, to the extent
+that they are valid under applicable law.  If additional permissions
+apply only to part of the Program, that part may be used separately
+under those permissions, but the entire Program remains governed by
+this License without regard to the additional permissions.
+
+  When you convey a copy of a covered work, you may at your option
+remove any additional permissions from that copy, or from any part of
+it.  (Additional permissions may be written to require their own
+removal in certain cases when you modify the work.)  You may place
+additional permissions on material, added by you to a covered work,
+for which you have or can give appropriate copyright permission.
+
+  Notwithstanding any other provision of this License, for material you
+add to a covered work, you may supplement the terms of this License with terms:
+
+    a) The voiding of section 10, such that the recipient of the Program
+    may be protected under the License.
+
+  All other non-permissive additional terms are considered "further
+restrictions" within the meaning of section 10.  If the Program as you
+received it, or any part of it, contains a notice stating that it is
+governed by this License along with a term that is a further
+restriction, you may remove that term.  If a license document contains
+a further restriction but permits relicensing or conveying under this
+License, you may add to a covered work material governed by the terms
+of that license document, provided that the further restriction does
+not survive such relicensing or conveying.
+
+  If you add terms to a covered work in accord with this section, you
+must place, in the relevant source files, a statement of the
+additional terms that apply to those files, or a notice indicating
+where to find the applicable terms.
+
+  Additional terms, permissive or non-permissive, may be stated in the
+form of a separately written license, or stated as exceptions;
+the above requirements apply either way.
+
+6. Termination
+  
+  You may not propagate or modify a covered work except as expressly
+provided under this License.  Any attempt otherwise to propagate or
+modify it is void, and will automatically terminate your rights under
+this License.
+
+  Termination of your rights under this section does not terminate the
+licenses of parties who have received copies or rights from you under
+this License, where appliable.
+
+7. Acceptance Not Required for Having Copies
+  You are not required to accept this License in order to receive or
+run a copy of the Program.  Ancillary propagation of a covered work
+occurring solely as a consequence of using peer-to-peer transmission
+to receive a copy likewise does not require acceptance.  However,
+nothing other than this License grants you permission to propagate or
+modify any covered work.  These actions infringe copyright if you do
+not accept this License.  Therefore, by modifying or propagating a
+covered work, you indicate your acceptance of this License to do so.
+
+8. Downstream Recipients
+
+  "Explicit" refers to the clear and precise representation of an object,
+that is written down in the source code, in documentation, comments or 
+otherwise.
+
+  Each time you convey a covered work, the recipient does not receive
+a license from the original licensors, to run, modify and propagate that work.
+
+  When you convey a covered work, the recipient may only run the work,
+unless you have an explicit section in which you allow recipients to modify.
+When this happens, the following terms supplement the License:
+    
+    a) The recipient may not convey the modified work at all;
+    b) The recipient must keep the modified copy to himself, and may 
+    not share the copy;
+    c) By modifying or propagating the specified section in which
+    explicit permission is granted to edit, the recipient automatically
+    accepts that the License is does not protcet him, and that his
+    modifications are subject to copyright laws.
+  
+  By modifying or propagating the work without your explicit permission,
+the recipient voids all rights to the work, and automatically accepts 
+you confiscating his copy. The recipient automatically agrees not to 
+run, modify, or propagante the modified work, and forfeits any and all
+rights and parts of the work to you.
+
+
+  9. No Surrender of Others' Freedom.
+
+  If conditions are imposed on you (whether by court order, agreement or
+otherwise) that contradict the conditions of this License, they do not
+excuse you from the conditions of this License.  If you cannot convey a
+covered work so as to satisfy simultaneously your obligations under this
+License and any other pertinent obligations, then as a consequence you may
+not convey it at all.  For example, if you agree to terms that obligate you
+to collect a royalty for further conveying from those to whom you convey
+the Program, the only way you could satisfy both those terms and this
+License would be to refrain entirely from conveying the Program.
+
+  10. Revised Versions of this License.
+
+  Robotics@Apex may publish revised and/or new versions of
+the Robotics@Apex License from time to time.  Such new versions will
+be similar in spirit to the present version, but may differ in detail to
+address new problems or concerns.
+
+  Each version is given a distinguishing version number.  If the
+Program specifies that a certain numbered version of the Robotics@Apex 
+License "or any later version" applies to it, you must follow the latest
+version published by Robotics@Apex. If the Program does not specify a 
+version number of the Robotics@Apex License, you must follow the latest
+version published by Robotics@Apex.
+
+  Later license versions may give you additional or different
+permissions. 
+
+  11. Disclaimer of Warranty.
+
+  THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+
+  12. Limitation of Liability.
+
+  IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS
+THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY
+GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE
+USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF
+DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD
+PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS),
+EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+  13. Interpretation of Sections 11 and 12.
+
+  If the disclaimer of warranty and limitation of liability provided
+above cannot be given local legal effect according to their terms,
+reviewing courts shall apply local law that most closely approximates
+an absolute waiver of all civil liability in connection with the
+Program, unless a warranty or assumption of liability accompanies a
+copy of the Program in return for a fee.
+
+                     END OF TERMS AND CONDITIONS
+
+            How to Apply These Terms to Your New Programs
+
+  If you develop a new program, and you want it to be of the greatest
+possible use to the public, the best way to achieve this is to make it
+free software which everyone can redistribute and change under these terms.
+
+  To do so, attach the following notices to the program.  It is safest
+to attach them to the start of each source file to most effectively
+state the exclusion of warranty; and each file should have at least
+the "copyright" line and a pointer to where the full notice is found.
+
+    "one line to give the program's name and a brief idea of what it does."
+    Copyright (C) "year"  "name of author"
+
+    This program is free software: you can run it, but not redistribute nor modify 
+    it under the terms of the latest Robotics@Apex License as published by
+    Robotics@Apex.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Robotics@Apex License for more details.
+
+    You should have received a copy of the Robotics@Apex License
+    along with this program.  If not, see <https://roboapex.github.io/licenses/>.
+
+Also add information on how to contact you by electronic and paper mail.
+
+  If the program does terminal interaction, make it output a short
+notice like this when it starts in an interactive mode:
+
+    "program"  Copyright (C) "year"  "name of author"
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+
+  The hypothetical commands `show w' and `show c' should show the appropriate
+parts of the Robotics@Apex License.  Of course, your program's commands
+might be different; for a GUI interface, you would use an "about box".
+      </pre>
+    </ThemeLayout>
+    // Runs, History, Accomplishments
+  );
+}
